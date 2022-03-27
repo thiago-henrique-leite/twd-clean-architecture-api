9acb5073c0d3a1931ac10249b7d77f26bd7c0f02
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-03-27 11:59:48 -0300

    feat: verify name when creating a user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,10 +1,18 @@
-import { Either, left } from '../share/either'
-import { InvalidEmailError } from './errors/invalid-email-error'
 import { UserData } from './user-data'
+import { Name } from './name'
 import { Email } from './email'
+import { Either, left } from '../share/either'
+import { InvalidNameError } from './errors/invalid-name-error'
+import { InvalidEmailError } from './errors/invalid-email-error'
 
 export class User {
-  static create (userData: UserData): Either<InvalidEmailError, User> {
+  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+    const nameOrError = Name.create(userData.name)
+
+    if (nameOrError.isLeft()) {
+      return left(new InvalidNameError())
+    }
+
     const emailOrError = Email.create(userData.email)
 
     if (emailOrError.isLeft()) {
