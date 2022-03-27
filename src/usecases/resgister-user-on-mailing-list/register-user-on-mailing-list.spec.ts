57c3874663141cda0754bf8d00f00c33dca8a706
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-03-27 12:54:46 -0300

    test: register valid user to mailing list and rename file

---
+++ src/usecases/resgister-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,18 @@
+import { UserData } from '../../entities/user-data'
+import { UserRepository } from './ports/user-repository'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
+import { RegisterUserOnMailingList } from './repository/register-user-on-mailing-list'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const response = await usecase.perform({ name, email })
+    const user = repo.findUserByEmail(email)
+    expect((await user).name).toBe(name)
+    expect(response.value.name).toBe(name)
+  })
+})
