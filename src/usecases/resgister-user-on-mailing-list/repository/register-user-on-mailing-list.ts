d579974efa31821b39f723a0d8e2420cba61be45
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-03-27 12:54:17 -0300

    feat: register valid user to mailing list

---
+++ src/usecases/resgister-user-on-mailing-list/repository/register-user-on-mailing-list.ts
@@ -0,0 +1,27 @@
+import { User } from '../../../entities/user'
+import { UserData } from '../../../entities/user-data'
+import { UserRepository } from '../ports/user-repository'
+import { Either, left, right } from '../../../share/either'
+import { InvalidNameError } from '../../../entities/errors/invalid-name-error'
+import { InvalidEmailError } from '../../../entities/errors/invalid-email-error'
+
+export class RegisterUserOnMailingList {
+  private readonly userRepo: UserRepository
+
+  constructor (userRepo: UserRepository) {
+    this.userRepo = userRepo
+  }
+
+  public async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request)
+    }
+    return right(request)
+  }
+}
